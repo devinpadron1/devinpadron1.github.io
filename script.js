989ea300a787aaa39769b3ada06af2da3092b9ea
Devin <devinpadron28@gmail.com>
2023-04-19 11:00:13 -0400

    Create custom elements for header and footer files in Javascript. Intent is to be able to use the same them across all webpages on the site.

---
+++ script.js
@@ -0,0 +1,30 @@
+// Create custom header element
+class myHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <header class="header">
+                <h1><a href="index.html">Devin Padrón</a></h1>
+                <div class="menu">
+                    <p>Projects</p>
+                    <p><a href="blog.html">Blog</a></p>
+                    <p><a href="aboutMe.html">About Me</a></p>
+                </div>
+            </header>
+        `
+    }
+}
+customElements.define('my-header', myHeader)
+
+// Create custom footer element
+class myFooter extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <section class="footer">
+                <a href="https://www.linkedin.com/in/devin-p/" target="_blank"><img src="pics/linkedin.png" alt="LinkedIn logo" class="logos"></a>
+                <a href="https://github.com/devinpadron1" target="_blank"><img src="pics/github.png" alt="GitHub logo" class="logos"></a>
+                <a href="https://twitter.com/devinpadron_" target="_blank"><img src="pics/twitter.png" alt="Twitter logo" class="logos"></a>
+            </section> 
+        `
+    }
+}
+customElements.define('my-footer', myFooter)
